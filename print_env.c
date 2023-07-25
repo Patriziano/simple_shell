0c388a338012524dad013afb37a519ace3226624
Vannyjoclear <ezejoseph96@gmail.com>
2023-07-25 16:20:34 -0700

    joe fixing print_env

--- print_env.c
+++ print_env.c
@@ -29,11 +29,11 @@ int printenv(char **args)
  */
 int _exitt(char **args)
 {
-	int exit_status = 0;
+	int status_exit = 0;
 
 	if (args[1] != NULL)
-		exit_status = atoi(args[1]);
-	exit(exit_status);
+		status_exit = atoi(args[1]);
+	exit(status_exit);
 }
 
 /**
