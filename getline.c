cba5ad91cc94054505dae4670c88ba3f8150f30b
Vannyjoclear <ezejoseph96@gmail.com>
2023-07-25 12:48:19 -0700

    joe fixing getline

--- getline.c
+++ getline.c
@@ -8,42 +8,42 @@
  */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 {
-	static char *buffer;
+	static char *buf;
 	size_t index = 0, bytes_read = 0, buf_size = BUFSIZ;
 	char *new_buf = NULL, ch;
 
 	if (!lineptr || !n || !stream)
 		return (-1);
-	buffer = malloc(buf_size);
+	buf = malloc(buf_size);
 	while (1)
 	{
 		if (bytes_read >= (buf_size - 1))
 		{
 			buf_size += BUFSIZ;
-			new_buf = _realloc(buffer, buf_size);
+			new_buf = _realloc(buf, buf_size);
 			if (!new_buf)
 			{
-				free(buffer);
-				buffer = NULL;
+				free(buf);
+				buf = NULL;
 				return (-1);
 			}
-			free(buffer);
-			buffer = new_buf;
+			free(buf);
+			buf = new_buf;
 		}
 		if ((read(fileno(stream), &ch, 1)) <= 0)
 		{
-			free(buffer);
-			buffer = NULL;
+			free(buf);
+			buf = NULL;
 			if (bytes_read > 0)
 				return (bytes_read);
 			return (-1);
 		}
-		buffer[index] = ch;
+		buf[index] = ch;
 		index++, bytes_read++;
-		if (buffer[index - 1] == '\n')
+		if (buf[index - 1] == '\n')
 			break;
 	}
-	buffer[index] = '\0';
-	*lineptr = buffer;
+	buf[index] = '\0';
+	*lineptr = buf;
 	return (bytes_read);
 }
