65448cfb2b623f125e37750560dbac84d30eb4fe
Vannyjoclear <ezejoseph96@gmail.com>
2023-07-25 14:23:30 -0700

    joe fixing tokenize

--- tokenize.c
+++ tokenize.c
@@ -9,20 +9,20 @@ void rm_spaces(char *p);
 char **tokenize(char *buffer, char *delim)
 {
 	int num_toks = 0, i = 0;
-	char *token = NULL, *buf_cp = NULL, **args = NULL;
+	char *token = NULL, *buf_cpy = NULL, **args = NULL;
 
 	while (*buffer == ' ')	/* remove preceding spaces */
 		buffer++;
 
-	buf_cp = my_strdup(buffer);
-	if (!buf_cp)
+	buf_cpy = my_strdup(buffer);
+	if (!buf_cpy)
 	{
 		perror("strdup");
 		free(buffer);
 		return (NULL);
 	}
-	rm_spaces(buf_cp);
-	token = _strtok(buf_cp, delim);
+	rm_spaces(buf_cpy);
+	token = _strtok(buf_cpy, delim);
 	while (token != NULL)	/* get number of tokens*/
 	{
 		num_toks++;
@@ -32,7 +32,7 @@ char **tokenize(char *buffer, char *delim)
 	if (!args)
 	{
 		perror("Error: malloc");
-		free(buf_cp);
+		free(buf_cpy);
 		free(buffer);
 		return (NULL);
 	}
@@ -43,7 +43,7 @@ char **tokenize(char *buffer, char *delim)
 		token = _strtok(NULL, delim);
 	}
 	args[i] = NULL;
-	free(buf_cp);
+	free(buf_cpy);
 	return (args);
 }
 
